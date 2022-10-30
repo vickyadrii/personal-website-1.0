6df9b69fb0dffde86683b707f1b5c9ae131b2cff
vickyadri29 <vickyadri290501@gmail.com>
2022-10-30 23:14:07 +0800

    style: styling reponsive skills on about pages

--- src/components/about/Experience.tsx
+++ src/components/about/Experience.tsx
@@ -8,25 +8,19 @@ const Experience = () => {
       <h2 className="md:text-4xl text-2xl md:text-left text-center font-bold text-primaryBlue dark:text-primaryPurple">
         Experience
       </h2>
-      <div className="flex flex-col items-end gap-3 md:mr-16 my-5 ">
+      <div className="flex flex-col md:items-end md:gap-3 gap-7 md:mr-16 my-5">
         {datas.map(({ id, company, date, title, desc }) => (
-          <div key={id} className="flex gap-5">
-            <div className="flex flex-col gap-1 md:items-end  md:max-w-xs">
-              <h3 className="md:text-xl text-xs font-bold text-right">
-                {company}
-              </h3>
-              <p className="md:text-sm text-xs md:text-left text-right">
-                {date}
-              </p>
+          <div key={id} className="flex md:flex-row flex-col md:gap-5 gap-1">
+            <div className="flex md:flex-col md:items-end items-center md:gap-1 gap-5  md:max-w-xs">
+              <h3 className="md:text-xl text-sm font-bold">{company}</h3>
+              <p className="md:text-sm text-xs">{date}</p>
             </div>
-            <div className="flex justify-center">
-              <div className="flex flex-col items-center">
-                <div className="w-5 h-5 bg-primaryPurple rounded-full" />
-                <div className="md:h-24 h-16 bg-primaryGreen w-1 rounded-b-full" />
-              </div>
+            <div className="md:flex hidden flex-col items-center">
+              <div className="w-5 h-5 bg-primaryPurple rounded-full" />
+              <div className="md:h-24 h-16 bg-primaryGreen w-1 rounded-b-full" />
             </div>
             <div className="flex flex-col gap-1 md:max-w-[400px]">
-              <h3 className="md:text-xl text-xs md:text-left text-right font-bold text-primaryGreen">
+              <h3 className="md:text-xl text-sm  font-bold text-primaryGreen">
                 {title}
               </h3>
               <p className="md:text-sm md:flex hidden text-xs md:max-w-sm font-medium">
--- src/components/about/Skills.tsx
+++ src/components/about/Skills.tsx
@@ -4,19 +4,21 @@ import { dataSkills } from "../../data";
 const Skills = () => {
   const { datas } = dataSkills;
   return (
-    <section className="bg-white dark:bg-slate-700 shadow-md rounded-[10px] p-5 md:w-[888px] md:h-[470px]">
-      <h2 className="md:text-4xl font-bold text-primaryBlue dark:text-primaryPurple">
+    <section className="bg-white dark:bg-slate-700 shadow-md rounded-[10px] p-5 md:w-[888px] md:h-[470px] h-[450px]">
+      <h2 className="md:text-4xl text-2xl md:text-left text-center font-bold text-primaryBlue dark:text-primaryPurple">
         Skills
       </h2>
       <div className="flex flex-col gap-5 my-5">
-        <div className="grid grid-cols-4 gap-x-16 gap-y-5 items-center m-auto">
+        <div className="grid md:grid-cols-4 grid-cols-3 md:gap-x-16 gap-x-5 md:gap-y-5 gap-y-4 items-center m-auto">
           {datas.map(({ id, image_url, title }) => (
             <div
               key={id}
-              className="flex justify-center items-center flex-col gap-2 w-[110px] h-[110px] p-3 bg-white dark:bg-slate-600 rounded-[10px] shadow-md hover:-translate-y-1 hover:drop-shadow-xl transition-all duration-200 ease-out"
+              className="flex justify-center items-center flex-col gap-2 md:w-[110px] w-[80px] md:h-[110px] h-[80px] p-3 bg-white dark:bg-slate-600 rounded-[10px] shadow-md hover:-translate-y-1 hover:drop-shadow-xl transition-all duration-200 ease-out"
             >
-              <img src={image_url} alt="Icon skills" className="w-12" />
-              <p className="text-sm font-medium text-primaryBlack dark:text-white">{title}</p>
+              <img src={image_url} alt="Icon skills" className="md:w-12 w-8" />
+              <p className="md:text-sm text-xs font-medium text-primaryBlack dark:text-white">
+                {title}
+              </p>
             </div>
           ))}
         </div>
